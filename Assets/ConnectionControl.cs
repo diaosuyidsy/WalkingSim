cbbe48cc80a3705db26f02e06afbd6110e304d21
Suyi Diao <suyi.diao@vanderbilt.edu>
2018-10-17 13:06:17 -0400

    initial commit

---
+++ Assets/ConnectionControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionControl : MonoBehaviour
+{
+    public Transform ConnectionPrePos1;
+    public Transform ConnectionPrePos2;
+    public Transform Connection1;
+    public Transform Connection2;
+    public GameObject EndingHolder;
+    public bool FirstEntry = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // If not first time entry, don't do anything
+            if (FirstEntry)
+            {
+                FirstEntry = false;
+                // We need to set all other hallways' FirstEntry true
+                setOtherHallwaysFirstEntryTrue();
+
+                // Connect other hallways
+                Connect(Connection1.position, ConnectionPrePos1);
+                Connect(Connection2.position, ConnectionPrePos2);
+
+                // If this is true, then need to display the ending
+                if (GameManager.GM.AddCount())
+                {
+                    Debug.Log("Set Ending True");
+                    EndingHolder.SetActive(true);
+                }
+            }
+        }
+    }
+
+    void Connect(Vector3 Pos, Transform PrePos)
+    {
+        // Get the correct Rotation
+        var connectionZ = Pos.z - PrePos.position.z;
+        var connectionX = Pos.x - PrePos.position.x;
+        var angle = Mathf.Atan2(connectionZ, connectionX);
+        var angleInDegree = Mathf.Rad2Deg * angle;
+        angleInDegree *= -1;
+
+        GameManager.GM.ChangeWall(Pos, angleInDegree);
+    }
+
+    public void setOtherHallwaysFirstEntryTrue()
+    {
+        for (int i = 0; i < GameManager.GM.HallwayAlternates.Length; i++)
+        {
+            if (GameManager.GM.HallwayAlternates[i] != transform.parent.gameObject)
+            {
+                GameManager.GM.HallwayAlternates[i].GetComponentInChildren<ConnectionControl>().FirstEntry = true;
+            }
+        }
+    }
+}
---
+++ Assets/EndWallControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndWallControl : MonoBehaviour
+{
+    public GameObject StrangeThing;
+    public GameObject theOtherWall;
+
+    private bool FirstTime = true;
+
+    public void RevealOtherWall()
+    {
+        if (!FirstTime)
+            return;
+        FirstTime = false;
+        theOtherWall.SetActive(false);
+        StrangeThing.SetActive(true);
+    }
+}
