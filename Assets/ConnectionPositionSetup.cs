696db716db8759a1259ea7d10119751b1ca7f40e
Suyi Diao <suyi.diao@vanderbilt.edu>
2018-10-18 12:18:00 -0400

    added models and textures:

---
+++ Assets/ConnectionPositionSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionPositionSetup : MonoBehaviour
+{
+
+    public Transform[] Walls;
+
+    void Start()
+    {
+        float temp = 0f;
+        for (int i = 0; i < Walls.Length; i++)
+        {
+            temp += Walls[i].position.z;
+        }
+        temp = temp / Walls.Length;
+        Debug.Log(temp);
+        transform.position = new Vector3(transform.position.x, transform.position.y, temp);
+    }
+}
