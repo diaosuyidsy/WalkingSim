adabadfdc0ca89864de69483b03887ca1cdf60b9
Suyi Diao <suyi.diao@vanderbilt.edu>
2018-11-05 23:39:10 -0500

    changed final glitch part

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.Audio;
 public class GameManager : MonoBehaviour
 {
     public static GameManager GM;
+    public GameObject Player;
     public int MaxCount = 5;
     public GameObject FirstHallway;
     public GameObject[] HallwayAlternates;
@@ -20,6 +21,9 @@ public class GameManager : MonoBehaviour
     private int count = 0;
     private bool StrangeThingShown = false;
     private int turnTimes = 2;
+    private float playerYRotation;
+    private bool playerStartGlitch = false;
+    private float maxDifference = 0f;
 
     private void Awake()
     {
@@ -51,7 +55,8 @@ public class GameManager : MonoBehaviour
                         {
                             hit1.transform.gameObject.GetComponent<EndWallControl>().RevealOtherWall();
                             StrangeThingShown = true;
-
+                            // Start the behavoir of glitch
+                            TurnGlitchStart();
                         }
                     }
                 }
@@ -67,6 +72,30 @@ public class GameManager : MonoBehaviour
             }
         }
 
+        if (playerStartGlitch)
+        {
+            float difference = Mathf.Abs(Player.transform.rotation.eulerAngles.y - playerYRotation);
+            Debug.Log("PlayerYRotation: " + playerYRotation);
+            Debug.Log(Player.transform.rotation.eulerAngles.y);
+            Debug.Log(maxDifference);
+            if (difference > maxDifference)
+            {
+                maxDifference = difference;
+                float percent = maxDifference / 120f;
+                Camera.main.GetComponent<Kino.AnalogGlitch>().scanLineJitter = 0.3f * percent;
+                Camera.main.GetComponent<Kino.AnalogGlitch>().verticalJump = 0.3f * percent;
+                Camera.main.GetComponent<Kino.AnalogGlitch>().horizontalShake = 0.3f * percent;
+                Camera.main.GetComponent<Kino.AnalogGlitch>().colorDrift = 0.3f * percent;
+
+            }
+        }
+
+    }
+
+    private void TurnGlitchStart()
+    {
+        playerYRotation = Player.transform.rotation.eulerAngles.y;
+        playerStartGlitch = true;
     }
 
     public bool AddCount()
